a0ba0712cc47b3fb62a1156e938939920d1e2e9f
ddul22 <kjj0757@naver.com>
2025-03-31 17:49:59 +0900

    03.31

--- vueworkspace/first_project/src/main.js
+++ vueworkspace/first_project/src/main.js
@@ -1,6 +1,10 @@
+// main.js
+// Root Vue 객체 생성위해 함수 가져오기
 import { createApp } from 'vue'
-// import App from './App.vue'
-import App from './components/CommonComponent.vue'
+// Root Vue 객체 정의파일 가져오기
+import App from './App.vue'
+// Vue Router 가져오기
+// import App from './components/CommonComponent.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
